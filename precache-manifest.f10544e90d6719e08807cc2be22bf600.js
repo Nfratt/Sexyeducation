b4cab500cd20f3e3f01dd7eacba4435cbeb7de75
Nick Fratt <nafratt@gmail.com>
2021-02-04 17:08:54 -0500

    Updates

---
+++ precache-manifest.f10544e90d6719e08807cc2be22bf600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140f9127cb4fb8fbb5f8b052ba036585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4df7526b74c84e82856f",
+    "url": "/static/css/main.cb665272.chunk.css"
+  },
+  {
+    "revision": "ee371aeec3aa8773ae29",
+    "url": "/static/js/2.88b272bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88b272bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df7526b74c84e82856f",
+    "url": "/static/js/main.1a6d5c5e.chunk.js"
+  },
+  {
+    "revision": "fa18ecbc5ecf4784e8db",
+    "url": "/static/js/runtime-main.abd54253.js"
+  },
+  {
+    "revision": "4d002cbf382c46726ca1548f8a3cbcbf",
+    "url": "/static/media/undraw_couplekiki.4d002cbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.984210e4418fb138362b10fb6287344a.js"
+  "/precache-manifest.f10544e90d6719e08807cc2be22bf600.js"
 );
 
 self.addEventListener('message', (event) => {
